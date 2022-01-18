920a9e30b2c0c687c625748ecc84f5b4075d52cd
anuzza <anuja3580@gmail.com>
2022-01-17 19:09:10 -0600

    scoping in practice

--- script.js
+++ script.js
@@ -28,8 +28,38 @@ the scope chain has nothing to do with the order in which  functions were called
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   console.log(firstName);
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      // reassigning outer scope's varaiable
+      output = 'New output!';
+    }
+    //console.log(str); no defined
+    console.log(millenial); // it can be defined if var is used
+
+    //console.log(add(2, 3));
+
+    console.log(output);
+  }
+
+  printAge();
+
   return age;
 }
 
 const firstName = 'Jonas';
 calcAge(1991);
+//console.log(age);
+//printAge(); cannot be defined here
